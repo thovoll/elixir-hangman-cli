d9bdd4f9fa91b4d47e8e19ebb256062939581b0f
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-13 17:19:17 -0700

    Print used letters.

--- lib/hangman_cli/summary.ex
+++ lib/hangman_cli/summary.ex
@@ -1,10 +1,13 @@
 defmodule HangmanCli.Summary do
   def display!(game = %{tally: tally}) do
+    #IO.inspect(game)
     IO.puts([
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}",
       "\n",
       "Guesses left: #{tally.turns_left}",
+      "\n",
+      "Letters used: #{Enum.join(tally.used, " ")}",
       "\n"
     ])
     game
