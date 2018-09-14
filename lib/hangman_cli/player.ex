2ba8608302cf718314452f9a5ca4cdd4d65afa9d
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-13 20:23:41 -0700

    Continue renaming game to state.

--- lib/hangman_cli/player.ex
+++ lib/hangman_cli/player.ex
@@ -11,33 +11,33 @@ defmodule HangmanCli.Player do
     exit_with_message("Sorry, you lost (the word was: #{word})")
   end
 
-  def play(game = %State{tally: %{game_state: :good_guess}}) do
-    continue_with_message(game, "Good guess!")
+  def play(state = %State{tally: %{game_state: :good_guess}}) do
+    continue_with_message(state, "Good guess!")
   end
 
-  def play(game = %State{tally: %{game_state: :bad_guess}}) do
-    continue_with_message(game, "Sorry, that isn't in the word")
+  def play(state = %State{tally: %{game_state: :bad_guess}}) do
+    continue_with_message(state, "Sorry, that isn't in the word")
   end
 
-  def play(game = %State{tally: %{game_state: :already_used}}) do
-    continue_with_message(game, "You've already used that letter")
+  def play(state = %State{tally: %{game_state: :already_used}}) do
+    continue_with_message(state, "You've already used that letter")
   end
 
-  def play(game) do
-    continue(game)
+  def play(state = %State{}) do
+    continue(state)
   end
 
-  def continue(game) do
-    game
+  def continue(state = %State{}) do
+    state
     |> Summary.display!()
     |> Prompter.accept_move()
     |> Mover.make_move()
     |> play()
   end
 
-  def continue_with_message(game, msg) do
+  def continue_with_message(state = %State{}, msg) do
     IO.puts(msg)
-    continue(game)
+    continue(state)
   end
 
   defp exit_with_message(msg) do
