1949f73500524891aa217d820ede2115cdf524a1
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-09-15 05:59:39 -0700

    Use case instead of many functions.

--- lib/hangman_cli/player.ex
+++ lib/hangman_cli/player.ex
@@ -1,31 +1,15 @@
 defmodule HangmanCli.Player do
   alias HangmanCli.{Mover, Prompter, State, Summary}
 
-  # won, lost, good guess, bad guess, already used, initializing
-  def play(state = %State{tally: %{game_state: :won}}) do
-    word = Enum.join(state.game_service.letters)
-    exit_with_message("#{word}! You WIN!")
-  end
-
-  def play(state = %State{tally: %{game_state: :lost}}) do
-    word = Enum.join(state.game_service.letters)
-    exit_with_message("Sorry, you lost (the word was: #{word})")
-  end
-
-  def play(state = %State{tally: %{game_state: :good_guess}}) do
-    continue_with_message(state, "Good guess!")
-  end
-
-  def play(state = %State{tally: %{game_state: :bad_guess}}) do
-    continue_with_message(state, "Sorry, that isn't in the word")
-  end
-
-  def play(state = %State{tally: %{game_state: :already_used}}) do
-    continue_with_message(state, "You've already used that letter")
-  end
-
   def play(state = %State{}) do
-    continue(state)
+    case state.tally.game_state do
+      :won -> exit_with_message("#{word(state)}! You WIN!")
+      :lost -> exit_with_message("Sorry, you lost (the word was: #{word(state)})")
+      :good_guess -> continue_with_message(state, "Good guess!")
+      :bad_guess -> continue_with_message(state, "Sorry, that isn't in the word")
+      :already_used -> continue_with_message(state, "You've already used that letter")
+      _ -> continue(state)
+    end
   end
 
   def continue(state = %State{}) do
@@ -44,5 +28,9 @@ defmodule HangmanCli.Player do
   defp exit_with_message(msg) do
     IO.puts(msg)
     exit(:normal)
+  end
+
+  defp word(state = %State{}) do
+    Enum.join(state.game_service.letters)
   end
 end
